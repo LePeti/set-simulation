4a639c3446108fe083aa76212e510a9df04c2688
Peter Lukacs <lukacs.peter.andras@gmail.com>
2022-01-30 19:09:00 +0000

    🍻 Add scratchpad

---
+++ src/scratchpad.py
@@ -0,0 +1,21 @@
+from main import *
+from random import seed
+
+
+seed(121)
+
+# a_set = np.array([
+#     [0, 0, 0, 0],
+#     [0, 1, 1, 2],
+#     [0, 2, 2, 1]
+# ])
+
+deck = generate_shuffled_deck()
+_, table = draw(deck, 12)
+print(f"number of cards on the table: {len(table)}")
+# table = np.concatenate([a_set, table])
+# print(table)
+sets = get_sets_on_table(table)
+print(f"sets type: {type(sets)}, shape: {sets.shape}")
+print(f"number of sets: {len(sets)}")
+print(sets)
