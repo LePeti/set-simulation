9bb41d7400af07142102b1ef676bbd51e3d1df2c
Peter Lukacs <lukacs.peter.andras@gmail.com>
2022-01-31 07:45:03 +0000

    ✨ Implement full game play

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
+import random
 from itertools import combinations, product
-from random import shuffle
 
 import numpy as np
 
 # TODO add logging
 
+random.seed(123)
+
 
 def generate_shuffled_deck():
     deck = list(product(range(3), repeat=4))
-    shuffle(deck)
+    random.shuffle(deck)
     return np.array(deck)
 
 
@@ -44,19 +46,40 @@ def add_one_card_to_table(deck, table):
     return deck, table
 
 
+def remove_set_from_table(table, set_):
+    indice_to_remove = []
+    for set_card in set_:
+        for i, card in enumerate(table):
+            if np.array_equal(card, set_card):
+                indice_to_remove.append(i)
+    return np.delete(table, indice_to_remove, axis=0)
+
+
 if __name__ == "__main__":
     deck = generate_shuffled_deck()
     deck, table = first_draw(deck)
+    removed_sets = []
+
     while len(deck) != 0:
         # find sets on table
         sets = get_sets_on_table(table)
         num_sets = len(sets)
         # if multiple sets, pick one arbitrarily and add 3 cards from deck
-        # if num_sets > 0:
+        if num_sets > 0:
+            chosen_set = random.choice(sets)
+            removed_sets.append(chosen_set)
+            table = remove_set_from_table(table, chosen_set)
+            deck, table = add_three_cards_to_table(deck, table)
+        else:
+            deck, table = add_one_card_to_table(deck, table)
 
-    # elif 0 sets, add 1 card from deck
-    # deck is empty, find remaining sets
+    remaining_sets_on_table = get_sets_on_table(table)
+    while len(remaining_sets_on_table) > 0:
+        chosen_set = random.choice(remaining_sets_on_table)
+        removed_sets.append(chosen_set)
+        table = remove_set_from_table(table, chosen_set)
+        remaining_sets_on_table = get_sets_on_table(table)
 
-    # num_sets = 0
-    # while num_sets == 0:
-    #     deck, table = add_one_card_to_table(deck, table)
+    print(f"Remaining cards in deck: {len(deck)}")
+    print(f"Remaining cards on table: {len(table)}")
+    print(f"Number of sets found: {len(removed_sets)} ({len(removed_sets) * 3} cards)")
