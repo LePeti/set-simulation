5a2d473a9e2cc752cae557829ccedc32078fd063
Peter Lukacs <lukacs.peter.andras@gmail.com>
2022-01-31 07:45:53 +0000

    🚧 scratchpad changes

--- src/scratchpad.py
+++ src/scratchpad.py
@@ -1,21 +1,22 @@
+import random
+
 from main import *
-from random import seed
 
+random.seed(121)
 
-seed(121)
-
-# a_set = np.array([
-#     [0, 0, 0, 0],
-#     [0, 1, 1, 2],
-#     [0, 2, 2, 1]
-# ])
+a_set = np.array([
+    [0, 0, 0, 0],
+    [0, 1, 1, 2],
+    [0, 2, 2, 1]
+])
 
 deck = generate_shuffled_deck()
-_, table = draw(deck, 2)
+_, table = draw(deck, 12)
 print(f"number of cards on the table: {len(table)}")
 table = np.concatenate([a_set, table])
 # print(table)
 sets = get_sets_on_table(table)
+print(f"table, set removed: {remove_set_from_table(table, np.squeeze(sets))}")
 print(f"sets type: {type(sets)}, shape: {sets.shape}")
 print(f"number of sets: {len(sets)}")
 print(sets)
