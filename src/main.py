6e4cede7ed68d816220f72b7e347bc6633b26e10
Peter Lukacs <lukacs.peter.andras@gmail.com>
2022-01-31 08:46:37 +0000

    🔊 Add some dummy logging to senity check the flow

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import logging
 import random
 from itertools import combinations, product
 
@@ -6,11 +7,13 @@ import numpy as np
 # TODO add logging
 
 random.seed(123)
+logging.basicConfig(level=logging.DEBUG)
 
 
 def generate_shuffled_deck():
     deck = list(product(range(3), repeat=4))
     random.shuffle(deck)
+    logging.info(f"Shuffled deck generated. {len(deck)} cards in deck.")
     return np.array(deck)
 
 
@@ -58,27 +61,40 @@ def remove_set_from_table(table, set_):
 if __name__ == "__main__":
     deck = generate_shuffled_deck()
     deck, table = first_draw(deck)
+    logging.info(f"{len(table)} cards on the table.")
     removed_sets = []
 
     while len(deck) != 0:
         # find sets on table
         sets = get_sets_on_table(table)
         num_sets = len(sets)
+        logging.info(f"Found {num_sets} sets on the table")
         # if multiple sets, pick one arbitrarily and add 3 cards from deck
         if num_sets > 0:
             chosen_set = random.choice(sets)
+            logging.info(f"Chosen set: \n {chosen_set} \n")
             removed_sets.append(chosen_set)
+            logging.info(f"Number of sets so far: {len(removed_sets)}")
             table = remove_set_from_table(table, chosen_set)
+            logging.info(f"Cards on the table: {len(table)}")
             deck, table = add_three_cards_to_table(deck, table)
+            logging.info(f"Adding new cards from deck. Num cards in deck/table: {len(deck)}/{len(table)}")
         else:
             deck, table = add_one_card_to_table(deck, table)
+    logging.info(f"No more cards in deck.")
+    logging.info(f"Remaining cards on table: {len(table)}")
 
+    logging.info(f"Looking for remaining sets on table.")
     remaining_sets_on_table = get_sets_on_table(table)
     while len(remaining_sets_on_table) > 0:
+        logging.info("Found one, looking for more.")
         chosen_set = random.choice(remaining_sets_on_table)
         removed_sets.append(chosen_set)
         table = remove_set_from_table(table, chosen_set)
         remaining_sets_on_table = get_sets_on_table(table)
+
+    logging.info(f"No more cards in deck, no more sets on table.")
+    logging.info(f"Remaining cards on table: {len(table)}")
 
     print(f"Remaining cards in deck: {len(deck)}")
     print(f"Remaining cards on table: {len(table)}")
