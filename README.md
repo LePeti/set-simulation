2a38829d77a02c6ae9e1b3dd5b9b38038d1321c1
Peter Lukacs <lukacs.peter.andras@gmail.com>
2022-02-02 21:27:41 +0000

    ✨ 🚧 Implement simulation data gathering

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # Set simulation
 
 > Set is a fun cardgame
-
 ## Game flow
 ![game-flow](game-flow.png)
+
+## Columns of SET simulation data
+
+1. `game_id` - the ID of a complete game of SET
+2. `round_id` - the ID of a round in SET which includes looking for SETs, removing one (if any) and replacing the removed cards from the deck (if any). `game_id` and `round_id` are the keys of the table.
+3. `round_type` - one of three types:
+   1. `normal` - deck not empty, at least 1 SET found
+   2. `no-set` - deck not empty, no SETs found
+   3. `no-deck` - deck empty, looking for set
+4. `is_first_round` - True if first round, False otherwise
+5. `is_last_round` - True if last round, False otherwise
+6. `date_time`
+8. `num_cards_table` - number of cards on the table at the beginning of the round
+9. `num_cards_deck` - number of cards in the deck at the beginning of the round
+10. `num_sets` - number of SETs at the beginning of the round
+11. `cards_table` - cards on the table
+12. `cards_deck` - cards in the deck
+13. `sets` - cards of SETs grouped
+14. `chosen_set`
--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import logging
 import random
+from datetime import datetime
 from itertools import combinations, product
 
+import matplotlib.ticker as mtick
 import numpy as np
+import pandas as pd
 from functions.draw_cards import draw_cards, translate_vecs_to_cards
+from matplotlib import pyplot as plt
 
 random.seed(123)
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 
 
 def generate_shuffled_deck():
@@ -28,6 +32,8 @@ def first_draw(deck):
 
 def get_sets_on_table(table):
     # TODO write tests
+    if len(table) == 0:
+        return np.array([])
     potential_sets = np.array(list(combinations(table, r=3)))
     attribute_sums = potential_sets.sum(axis=1)
     attribute_level_check = np.isin(attribute_sums, [0, 3, 6])
@@ -51,51 +57,153 @@ def remove_set_from_table(table, set_):
     return np.delete(table, indice_to_remove, axis=0)
 
 
+def get_simul_data_col_names():
+    return [
+        "game_id",
+        "round_id",
+        "round_type",
+        "is_first_round",
+        "is_last_round",
+        "datetime",
+        "num_cards_table",
+        "num_cards_deck",
+        "num_sets",
+        "cards_table",
+        "cards_deck",
+        "sets",
+        "chosen_set",
+    ]
+
+
 if __name__ == "__main__":
-    deck = generate_shuffled_deck()
-    deck, table = first_draw(deck)
-    logging.info(f"{len(table)} cards on the table.")
-    removed_sets = []
-    round_ = 0
+    simul_data = pd.DataFrame(columns=get_simul_data_col_names())
+    game_id = 0
+    for game in range(100):
+        logging.warning(f"Game simulation round #{game} ({game:.0f}%)")
+        game_id += 1
+        deck = generate_shuffled_deck()
+        deck, table = first_draw(deck)
+        logging.info(f"{len(table)} cards on the table.")
 
-    while len(deck) != 0:
-        round_ += 1
-        logging.info(f"\n\n-~=ROUND #{round_}=~-\n")
-        logging.info(f"Num cards in deck/table: {len(deck)}/{len(table)}.")
-        sets = get_sets_on_table(table)
-        num_sets = len(sets)
-        logging.info(f"Found {num_sets} SET(s) on the table.")
-        if num_sets > 0:
-            chosen_set = random.choice(sets)
-            draw_cards(translate_vecs_to_cards(chosen_set))
-            removed_sets.append(chosen_set)
-            table = remove_set_from_table(table, chosen_set)
-            deck, table = add_n_cards_to_table(deck, table, 12 - len(table))
-            logging.info(
-                f"Chosen set: \n {chosen_set} \n\n"
-                f"(total SETs so far: {len(removed_sets)})"
+        removed_sets = []
+        game_data = pd.DataFrame(columns=get_simul_data_col_names())
+        is_last_round = False
+        round_id = 0
+
+        while len(deck) != 0:
+            round_id += 1
+            logging.info(f"\n\n-~=ROUND #{round_id}=~-\n")
+            logging.info(f"Num cards in deck/table: {len(deck)}/{len(table)}.")
+            # TODO rename to sets_on_table (also in dataframe)
+            sets = get_sets_on_table(table)
+            num_sets = len(sets)
+            logging.info(f"Found {num_sets} SET(s) on the table.")
+            round_data = pd.DataFrame(
+                {
+                    "game_id": [game_id],
+                    "round_id": [round_id],
+                    "is_first_round": [round_id == 1],
+                    "is_last_round": [is_last_round],
+                    "datetime": [datetime.utcnow().isoformat()],
+                    "num_cards_table": [len(table)],
+                    "num_cards_deck": [len(deck)],
+                    "num_sets": [num_sets],
+                    "cards_table": [[table]],
+                    "cards_deck": [[deck]],
+                    "sets": [[sets]],
+                }
             )
-            logging.info("Adding new cards from deck.")
-        else:
-            deck, table = add_n_cards_to_table(deck, table, 1)
-            logging.info("No sets found on the table, adding 1 card.")
-    logging.info("No more cards in deck.")
-    logging.info(f"Remaining cards on table: {len(table)}.")
+            if num_sets > 0:
+                round_data["round_type"] = "normal"
+                chosen_set = random.choice(sets)
+                round_data["chosen_set"] = [chosen_set]
+                draw_cards(translate_vecs_to_cards(chosen_set))
+                removed_sets.append(chosen_set)
+                table = remove_set_from_table(table, chosen_set)
+                deck, table = add_n_cards_to_table(deck, table, 12 - len(table))
+                logging.info(
+                    f"Chosen set: \n {chosen_set} \n\n"
+                    f"(total SETs so far: {len(removed_sets)})"
+                )
+                logging.info("Adding new cards from deck.")
+            else:
+                round_data["round_type"] = "no-set"
+                round_data["chosen_set"] = [sets]
+                deck, table = add_n_cards_to_table(deck, table, 1)
+                logging.info("No sets found on the table, adding 1 card.")
+            game_data = pd.concat([game_data, round_data], ignore_index=True)
 
-    logging.info("Looking for remaining sets on table.")
-    remaining_sets_on_table = get_sets_on_table(table)
-    while len(remaining_sets_on_table) > 0:
-        logging.info("Found one, looking for more.")
-        chosen_set = random.choice(remaining_sets_on_table)
-        removed_sets.append(chosen_set)
-        table = remove_set_from_table(table, chosen_set)
+        logging.info("No more cards in deck.")
+        logging.info(f"Remaining cards on table: {len(table)}.")
+        logging.info("Looking for remaining sets on table.")
         remaining_sets_on_table = get_sets_on_table(table)
 
-    draw_cards(translate_vecs_to_cards(table), "Cards left on the table w/o any SETs")
-    logging.info("No more cards in deck, no more sets on table.")
+        while len(remaining_sets_on_table) > 0:
+            round_id += 1
+            round_data = pd.DataFrame(
+                {
+                    "game_id": [game_id],
+                    "round_id": [round_id],
+                    "is_first_round": [round_id == 1],
+                    "is_last_round": [False],
+                    "datetime": [datetime.utcnow().isoformat()],
+                    "num_cards_table": [len(table)],
+                    "num_cards_deck": [len(deck)],
+                    "num_sets": [len(remaining_sets_on_table)],
+                    "cards_table": [[table]],
+                    "cards_deck": [[deck]],
+                    "sets": [[remaining_sets_on_table]],
+                }
+            )
+            logging.info("Found one, looking for more.")
+            chosen_set = random.choice(remaining_sets_on_table)
+            removed_sets.append(chosen_set)
+            table = remove_set_from_table(table, chosen_set)
+            remaining_sets_on_table = get_sets_on_table(table)
+            round_data["chosen_set"] = [chosen_set]
+            round_data["round_type"] = "no-deck"
+            game_data = pd.concat([game_data, round_data], ignore_index=True)
 
-    logging.info(f"Remaining cards in deck: {len(deck)}.")
-    logging.info(f"Remaining cards on table: {len(table)}.")
-    logging.info(
-        f"Number of sets found: {len(removed_sets)} " f"({len(removed_sets) * 3} cards.)"
-    )
+        round_id += 1
+        round_data = pd.DataFrame(
+            {
+                "game_id": [game_id],
+                "round_id": [round_id],
+                "is_first_round": [round_id == 1],
+                "round_type": ["no-deck"],
+                "datetime": [datetime.utcnow().isoformat()],
+                "num_cards_table": [len(table)],
+                "num_cards_deck": [len(deck)],
+                "num_sets": 0,
+                "cards_table": [[table]],
+                "cards_deck": [[deck]],
+                "sets": [[remaining_sets_on_table]],
+            }
+        )
+        round_data["is_last_round"] = True
+        round_data["chosen_set"] = [remaining_sets_on_table]
+        game_data = pd.concat([game_data, round_data], ignore_index=True)
+
+        draw_cards(
+            translate_vecs_to_cards(table), "Cards left on the table w/o any SETs"
+        )
+        logging.info("No more cards in deck, no more sets on table.")
+
+        logging.info(f"Remaining cards in deck: {len(deck)}.")
+        logging.info(f"Remaining cards on table: {len(table)}.")
+        logging.info(
+            f"Number of sets found: {len(removed_sets)} "
+            f"({len(removed_sets) * 3} cards.)"
+        )
+        simul_data = pd.concat([simul_data, game_data], ignore_index=True)
+
+
+(
+    simul_data.query("num_cards_table >= 12")["num_sets"]
+    .value_counts(normalize=True)
+    .reset_index()
+    .query("num_sets >= 0.01")
+    .plot(kind="bar", x="index", y="num_sets")
+)
+ax = plt.gca()
+ax.yaxis.set_major_formatter(mtick.PercentFormatter(1, decimals=0))
